3c7eb8d3e19759ccc37d1b9e69615bb7cab23669
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-01-07 17:56:18 -0500

    openv/core: update, no instantiation so import doesn't trigger auth

--- openv/core.py
+++ openv/core.py
@@ -1,4 +1,5 @@
 import os
+from typing import Optional
 from onepassword import OnePassword
 from openv.constant import DOTENV_VAULT
 from openv.exception import (
@@ -8,17 +9,14 @@ from openv.exception import (
 )
 
 
-op = OnePassword()
-
-
-def validate_dotenv_vault_exists() -> None:
+def validate_dotenv_vault_exists(op: OnePassword) -> None:
     "Check the .env vault exists."
     vault_names = [vault["name"] for vault in op.list_vaults()]
-    if not DOTENV_VAULT not in vault_names:
+    if DOTENV_VAULT not in vault_names:
         raise VaultNotFoundError(vault=DOTENV_VAULT)
 
 
-def get_uuid_for_project(project: str) -> str:
+def get_uuid_for_project(project: str, op: OnePassword) -> str:
     uuids = [
         item["id"]
         for item in op.list_items(vault=DOTENV_VAULT)
@@ -31,7 +29,7 @@ def get_uuid_for_project(project: str) -> str:
     return uuids[0]
 
 
-def get_fields_for_project(uuid: str) -> dict[str, str]:
+def get_fields_for_project(uuid: str, op: OnePassword) -> dict[str, str]:
     return {
         field["label"]: field["value"]
         for field in op.get_item(uuid=uuid)["fields"]
@@ -39,14 +37,15 @@ def get_fields_for_project(uuid: str) -> dict[str, str]:
     }
 
 
-def set_environment_variables(fields: dict[str, str]) -> None:
+def set_environment_variables(fields: dict[str, str], op: OnePassword) -> None:
     for key, val in fields.items():
         if val is not None:
             os.environ[key] = val
 
 
 def load_openv(project: str) -> None:
-    validate_dotenv_vault_exists()
-    project_uuid = get_uuid_for_project(project=project)
-    fields = get_fields_for_project(uuid=project_uuid)
-    set_environment_variables(fields=fields)
+    op = OnePassword()
+    validate_dotenv_vault_exists(op=op)
+    project_uuid = get_uuid_for_project(project=project, op=op)
+    fields = get_fields_for_project(uuid=project_uuid, op=op)
+    set_environment_variables(fields=fields, op=op)
